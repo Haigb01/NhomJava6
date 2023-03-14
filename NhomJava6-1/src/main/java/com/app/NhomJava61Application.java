423c763cdcca80b159a112bda52da7d3b3fdcfa4
hai cao <haigb01@gmail.com>
2023-03-14 22:32:03 +0700

    Create a new project

---
+++ NhomJava6-1/src/main/java/com/app/NhomJava61Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhomJava61Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhomJava61Application.class, args);
+	}
+
+}
---
+++ NhomJava6-1/src/test/java/com/app/NhomJava61ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhomJava61ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
